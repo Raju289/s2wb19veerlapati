6371df15692ce9484603a709015a224330aed69d
Raju289 <raju.veerlapati289@gmail.com>
2022-04-06 15:31:41 -0500

    created article

--- routes/article.js
+++ routes/article.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Article', { title: 'Search Results for Article' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
