41122ae632e29e59c9b8b35512ff9f63205c279e
Raju289 <raju.veerlapati289@gmail.com>
2022-04-06 20:32:04 -0500

    updated bonus

--- routes/electronics.js
+++ routes/electronics.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('electronics', { title: 'search results for electronics' });
 });
 
 module.exports = router;
